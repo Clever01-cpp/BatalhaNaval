4c351d60e73b24dc88e767af82b697b7203d799c
Cléver <rian.clever@gmail.com>
2026-03-29 13:30:13 -0300

    codigo comentado

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from src import constants
 from src import functions
 from sys import exit
 
+#função principal do jogo, onde a maioria da lógica acontece, no caso, aqui ainda estou trabalhando alternancia de telas, cliques do jogador e etc, ainda estou trabalhando com isso, por enquanto estou apenas alternando as cores, mas posteriormente quero colocar uma imagem de submarinos
 def main():
+
     pygame.init()
 
     screen = pygame.display.set_mode((constants.LARGURA_TELA, constants.ALTURA_TELA))
